f4a1bd56765959f46717d560977307c6bcac3cb1
Flávio Novaes de Oliveira <flavio.novaes29@gmail.com>
2026-03-01 15:29:57 -0300

    feat: criacao variavel aviso para input vazio

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function gerarTabuada() {
     var numero = document.getElementById('numero')
     var resultado = document.getElementById('areatabuada')
+    var aviso = document.getElementById('aviso')
 
     var n = Number(numero.value)
     resultado.value = ''
     count = 0
 
-    while (count <= 10) {
-        var multiplicacao = n * count
-        resultado.value += `${n} x ${count} = ${multiplicacao} \n`
-        count++
+    if (numero.value.length == 0) {
+        aviso.innerHTML = `<strong>Digite um número abaixo!</strong>`
+    } else {
+        while (count <= 10) {
+            aviso.innerHTML = ''
+            var multiplicacao = n * count
+            resultado.value += `${n} x ${count} = ${multiplicacao} \n`
+            count++
+        }
     }
 
     // for (var count = 0; count <= 10; count++) {
